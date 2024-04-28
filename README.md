b7233ea2121384ac1ca2a0221027db596778a71b
Matt Bosack <matt@secured-designs.local>
2024-04-28 06:44:46 -0400

    add -i and -o cli argparse

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # qr-code-gen
 Take a list of links and generate individual QR code images for them
+
+# TODO
+[x] allow for use input at cli
+    [] make this more robust
+[] menu
+[] zip up the file
+[] email the file
+[] auto test all QR codes
--- codegen.py
+++ codegen.py
@@ -1,8 +1,10 @@
+import sys, getopt
 import csv
 import qrcode
 
-def readAndPrintFile(fileName):
-    with open(fileName) as csvFile:
+
+def readAndPrintFile(inputFileName, outputFilename):
+    with open(inputFileName) as csvFile:
         qrReader = csv.reader(csvFile, delimiter=",")
         for row in qrReader:
             thisQRfilename=row[0]
@@ -15,10 +17,22 @@ def genQRcode(thisQRpair):
     myImageName = thisQRpair[0]+".png"
     img.save(dir+myImageName)
 
-def main():
-    fileName = 'code-list.csv'
-    thisQRpair = readAndPrintFile(fileName)
-    #genQRcode(thisQRpair)
+def main(argv):
+    inputFileName = 'code-list.csv'
+    outputFilename = 'qr-codes.zip'
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["input=","output"])
+    except getopt.GetoptError:
+        print("codegen.py -i <inputfile> -o <outputfile")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            print("codegen.py -i <inputfile> -o <outputfile")
+        elif opt in ("-i", "--input"):
+            inputFilename = arg
+        elif opt in ('-o', "--ofile"):
+            outputFilename = arg
+    thisQRpair = readAndPrintFile(inputFileName, outputFilename)
 
 if __name__ == '__main__':
-    main()
+    main((sys.argv[1:]))
